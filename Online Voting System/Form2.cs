bbc4e2f143c3a124d4dba194c6c7a46d25a20073
AyeshaMalik07 <afomalik86@gmail.com>
2023-12-04 22:24:41 +0500

    Updates (no new form)

--- Online Voting System/Form2.cs
+++ Online Voting System/Form2.cs
@@ -1,20 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Online_Voting_System
 {
     public partial class Form2 : Form
     {
+        String ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\afoma\OneDrive\Documents\voting db.mdf"";Integrated Security=True;Connect Timeout=30";
         public Form2()
         {
             InitializeComponent();
+            String[] Months = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+            comboBox2.Items.AddRange(Months);
+
+            for (int i = 1; i <= 31; i++)
+            {
+                comboBox1.Items.Add(i);
+            }
+
+            for (int i = 1900; i <= 2005; i++)
+            {
+                comboBox3.Items.Add(i);
+            }
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
@@ -25,7 +32,7 @@ namespace Online_Voting_System
         private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
 
-           
+
         }
 
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
@@ -33,6 +40,11 @@ namespace Online_Voting_System
             Form1 F1 = new Form1();
             F1.Show();
             this.Hide();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
---
+++ SQLQuery2.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Candidates]
+(
+    Candidate_ID INT PRIMARY KEY NOT NULL,
+    NA_ID INT NULL,
+    Candidate_Name VARCHAR(50) NULL,
+    Party VARCHAR(50) NULL,
+    Picture IMAGE NULL,
+    NA_Name VARCHAR(50) NULL,
+    FOREIGN KEY (NA_ID) REFERENCES [dbo].[NA](NA_ID)
+);
+
+
+
+
